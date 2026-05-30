8e502cb2e936c79192a9ad110abe1551f9b4afd2
Ismail <ismail.cg373@gmail.com>
2026-05-30 22:46:24 +0530

    feat: initialize CircleNet backend project

---
+++ cn-backend/src/main/java/in/horyezun/CircleNetApplication.java
@@ -0,0 +1,13 @@
+package in.horyezun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleNetApplication.class, args);
+	}
+	
+}
