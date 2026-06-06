90aae444c79b93c032be6286b2ab26486768c357
Ismail <ismail.cg373@gmail.com>
2026-06-06 17:11:12 +0530

    feat: initialize React frontend with Vite and Bootstrap

---
+++ cn-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App () {
+    return (
+        <div className="container mt-5">
+            <h1>CircleNet</h1>
+
+            <p>
+                Welcome to CircleNet.
+            </p>
+        </div>
+    );
+}
+
+export default App;
