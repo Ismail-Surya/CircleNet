cd5e0c3cf1eb3ddb016d338c9af558ea0c1f601f
Ismail <ismail.cg373@gmail.com>
2026-06-21 14:34:30 +0530

    Add JWT parsing utilities and CustomUserDetailsService

--- cn-backend/src/main/java/in/horyezun/security/jwt/JwtService.java
+++ cn-backend/src/main/java/in/horyezun/security/jwt/JwtService.java
@@ -3,9 +3,12 @@ package in.horyezun.security.jwt;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -41,6 +44,54 @@ public class JwtService {
 					getSigningKey()
 				)
 				.compact();
+	}
+	
+	private Claims extractAllClaims (String token) {
+		return Jwts.parserBuilder().
+				setSigningKey(
+						getSigningKey())
+					.build()
+					.parseClaimsJws(token)
+					.getBody();
+	}
+	
+	public String extractUsername (String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+
+	private <T> T extractClaim(String token, Function <Claims, T> resolver) {
+		
+		Claims claims = extractAllClaims(token);
+		return resolver.apply(claims);
+	}
+	
+	public Date extractExpiration (String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+	
+	/* public boolean isTokenValid(String token, String username) {
+		String extractedUsername = extractUsername(token);
+		
+		return extractedUsername.equals(username)
+				&& !isTokenExpired(token);
+	} */
+	
+	// Below is the alternate implementation for isTokenValid (String token, String username)
+	
+	public boolean isTokenValid(String token, String username) {
+		try {
+		String extractedUsername = extractUsername (token);
+		
+		return extractedUsername.equals(username);
+		} catch (JwtException exc) {
+			return false;
+		}
+		
+	}
+
+	private boolean isTokenExpired(String token) {
+		
+		return extractExpiration(token).before(new Date());
 	}
 	
 }
---
+++ cn-backend/src/main/java/in/horyezun/security/service/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package in.horyezun.security.service;
+
+import java.util.Collections;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import in.horyezun.user.entity.User;
+import in.horyezun.user.service.UserService;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private final UserService userService;
+	
+	public CustomUserDetailsService (UserService userService) {
+		this.userService = userService;
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user =
+				userService.findByUsername(
+							username
+						)
+						.orElseThrow(
+									() ->
+										new UsernameNotFoundException("User not found")
+								);
+		
+		return new org.springframework.security.core.userdetails.User(
+				user.getUsername(),
+				user.getPassword(),
+				Collections.emptyList()
+				);
+	}
+
+	
+	
+}
